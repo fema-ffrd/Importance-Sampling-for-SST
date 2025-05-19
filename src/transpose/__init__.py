ad472b9ee5fb2ffe683845c34a83482e2ba54238
shubhamjain15 <shubham.jain@mbakerintl.com>
2025-05-19 13:06:03 +0000

    Add LHS sampling to example notebook

--- src/transpose/__init__.py
+++ src/transpose/__init__.py
@@ -2,9 +2,13 @@ from .transposer import  transpose_gdf
 from .transposer import transpose_storm
 from .precipdepths import compute_depths
 from .sampling import sample_storms
+from .sampling import sample_storms_lhs
+from .sampling import sample_storms_lhs_equally
 
 __all__ = ["transpose_gdf",
            "transpose_storm",
            "compute_depths",
-           "sample_storms"
+           "sample_storms",
+           "sample_storms_lhs",
+           "sample_storms_lhs_equally"
 ]
--- src/transpose/sampling.py
+++ src/transpose/sampling.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from scipy.stats import uniform
 import geopandas as gpd
-
+from pyDOE2 import lhs
 
 def sample_storm_centers(v_domain_stats: pd.Series, dist_x=None, dist_y=None, num_simulations=10000):
     if dist_x is None:
@@ -57,6 +57,49 @@ def sample_storm_centers(v_domain_stats: pd.Series, dist_x=None, dist_y=None, nu
     return df
 
 
+def sample_storm_centers_lhs(v_domain_stats: pd.Series, dist_x=None, dist_y=None, num_simulations=10000):
+    xmin, ymin = v_domain_stats.minx, v_domain_stats.miny
+    xrange, yrange = v_domain_stats.range_x, v_domain_stats.range_y
+
+    if dist_x is None:
+        # Uniform sampling
+        dist_x = uniform(xmin, xrange)
+        dist_y = uniform(ymin, yrange)
+
+        v_centroid_x = dist_x.rvs(num_simulations)
+        v_centroid_y = dist_y.rvs(num_simulations)
+        v_weight = 1
+        v_weight_norm = 1 / num_simulations
+
+    else:
+        # LHS for truncated normal
+        unit_samples = lhs(2, samples=num_simulations)
+
+        def lhs_truncnorm(unit_vals, dist):
+            values = dist.ppf(unit_vals)
+            pdf_vals = dist.pdf(values)
+            return values, pdf_vals
+
+        v_centroid_x, f_x_vals = lhs_truncnorm(unit_samples[:, 0], dist_x)
+        v_centroid_y, f_y_vals = lhs_truncnorm(unit_samples[:, 1], dist_y)
+
+        # Importance sampling weights
+        f_X_U = 1 / xrange
+        f_Y_U = 1 / yrange
+        p = f_X_U * f_Y_U
+        q = f_x_vals * f_y_vals
+        v_weight = np.where(q > 0, p / q, 0)
+        v_weight_norm = v_weight / np.sum(v_weight)
+
+    return pd.DataFrame({
+        "x_sampled": v_centroid_x,
+        "y_sampled": v_centroid_y,
+        "prob": v_weight_norm,
+        "weight": v_weight,
+    })
+
+
+
 #Get storm catalogue samples
 def sample_storm_catalogues(df_storms: pd.DataFrame, num_simulations=10000) -> pd.DataFrame:
     return df_storms.sample(num_simulations, replace=True).reset_index(drop=True)
@@ -67,6 +110,51 @@ def sample_storms(df_storms: pd.DataFrame, v_domain_stats: pd.Series, dist_x=Non
     
     tqdm._instances.clear()
     _df_storm_centers = sample_storm_centers(v_domain_stats=v_domain_stats, dist_x=dist_x, dist_y=dist_y, num_simulations=num_simulations)
+    
+    df_storm_sample = \
+    (pd.concat([_df_storm_sample, _df_storm_centers], axis=1)
+        .assign(x_del = lambda _: _.x_sampled - _.x)
+        .assign(y_del = lambda _: _.y_sampled - _.y)
+    )
+
+    return df_storm_sample
+
+
+def sample_storms_lhs(df_storms: pd.DataFrame, v_domain_stats: pd.Series, dist_x=None, dist_y=None, num_simulations=10000):
+    _df_storm_sample = sample_storm_catalogues(df_storms=df_storms, num_simulations=num_simulations)
+    
+    tqdm._instances.clear()
+    _df_storm_centers = sample_storm_centers_lhs(v_domain_stats=v_domain_stats, dist_x=dist_x, dist_y=dist_y, num_simulations=num_simulations)
+    
+    df_storm_sample = \
+    (pd.concat([_df_storm_sample, _df_storm_centers], axis=1)
+        .assign(x_del = lambda _: _.x_sampled - _.x)
+        .assign(y_del = lambda _: _.y_sampled - _.y)
+    )
+
+    return df_storm_sample
+
+
+def sample_storms_lhs_equally(df_storms: pd.DataFrame, v_domain_stats: pd.Series, dist_x=None, dist_y=None, num_simulations=10000):
+    unique_storms = df_storms.reset_index(drop=True)
+    n_storms = len(unique_storms)
+
+    if num_simulations < n_storms:
+        raise ValueError(f"num_simulations ({num_simulations}) must be ≥ number of unique storms ({n_storms})")
+
+    reps = num_simulations // n_storms
+    remainder = num_simulations % n_storms
+
+    repeated = pd.concat([unique_storms] * reps, ignore_index=True)
+
+    if remainder > 0:
+        extra = unique_storms.sample(remainder, replace=False)
+        repeated = pd.concat([repeated, extra], ignore_index=True)
+
+    _df_storm_sample = repeated
+    
+    tqdm._instances.clear()
+    _df_storm_centers = sample_storm_centers_lhs(v_domain_stats=v_domain_stats, dist_x=dist_x, dist_y=dist_y, num_simulations=num_simulations)
     
     df_storm_sample = \
     (pd.concat([_df_storm_sample, _df_storm_centers], axis=1)
